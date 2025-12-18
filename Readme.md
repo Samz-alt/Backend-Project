ec6b6390591ba462756f24ab9a11323eb5b32db7
Saumz <bhowmicksaumyajeet@gmail.com>
2025-12-19 01:01:08 +0530

    add user model and wrote prototype functions, pre hook and video model and  setup mongooseAggregatePaginate

--- Readme.md
+++ Readme.md
@@ -1,1 +1,10 @@
 This is a learning Project based path for learning Backend Development
+
+"bcryptjs": used for encryption of the password by hashing the password and many other functions.
+"cookie-parser": used for parsing the cookies.
+"cors": used to solve cors origin problem.
+"dotenv": used to load .env's as soon as the files load.
+"express": software used for routing and all.
+"jsonwebtoken": used to generate auth tokens.
+"mongoose": connector used to connect and talk to for MongoDB.
+"mongoose-aggregate-paginate-v2": 
---
+++ src/models/user.model.js
@@ -0,0 +1,91 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    fullName: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true
+    },
+    avatar: {
+        type: String, //cloudnary bucket URL
+        required: true
+    },
+    coverImage: {
+        type: String,
+    },
+    password: {
+        type: String,
+        required: [true, "password is required"]
+    },
+    refreshToken: {
+        type: String,
+    },
+    watchHistory: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video"
+    }
+}, { timestamps: true })
+
+//middleware hook for encrypting the password before saving it to the DB
+userSchema.pre("save", async function () {
+    if (!this.isModified("password")) return  //here "this" is the mongoose document or we know as schema
+
+    this.password = await bcrypt.hash(this.password, 10) //this means before saving the password encrypt it to hash
+
+})
+
+//assigning method in the schema for checking the password when the user sends requests for login or different purposes
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+//assigning methods(prototypes OOP) in the schema for jwt tokens
+userSchema.methods.generateAccessToken = function () {
+    jwt.sign(
+        {
+            _id: this._id,
+            username: this.username,
+            email: this.email,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function () {
+    jwt.sign(
+        {
+            _id: this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+
+
+
+export const User = mongoose.model("User", userSchema)
