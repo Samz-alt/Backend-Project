34d54fadb069c98636586e8f625b050498f25ab4
Saumz <bhowmicksaumyajeet@gmail.com>
2025-12-21 19:33:25 +0530

    add await in asyncHandler and a description in multer

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -2,7 +2,7 @@ import multer from "multer"
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, '../../public/temp')
+        cb(null, '../../public/temp') //if we want to pass an error instance for constructor then first
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname)
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,6 +1,6 @@
 const asyncHandler = (requestHandler) => async (req, res, next) => {
     try {
-        requestHandler(req, res, next)  //here the variable that stores the function reference has been called and executed with the params coming from the express.
+        return await requestHandler(req, res, next)  //here the variable that stores the function reference has been called and executed with the params coming from the express.
     } catch (error) {
         res.status(error.code || 500).json({
             success: false,
